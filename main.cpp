7787c8fcd2c2057790689a0ec6ad574018908537
falconice <falcon@ice.com>
2022-12-24 20:53:14 +0300

    Добавить README.md.

---
+++ main.cpp
@@ -0,0 +1,7 @@
+﻿// lab-3-QuickSort.cpp : Этот файл содержит функцию "main". Здесь начинается и
+// заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main() { std::cout << "Hello World!\n"; }
